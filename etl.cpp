f1b822cf32fc5c8d434ffa80e393aed4f841ef66
Brian H. Ward <glyphrider@gmail.com>
2016-08-19 11:02:11 -0400

    First test passing

--- etl.cpp
+++ etl.cpp
@@ -9,9 +9,9 @@ namespace etl
 		std::map<char,int> result;
 		for(std::map<int,std::vector<char>>::const_iterator it = legacy.begin(); it != legacy.end(); it++)
 		{
-			for(vector<char>::const_iterator it2 = it->second().begin(); it2 != it.second().end(); it2++)
+		  for(std::vector<char>::const_iterator it2 = it->second.begin(); it2 != it->second.end(); it2++)
 			{
-				result[tolower(it->first())] = *it2;
+			  result[tolower(*it2)] = it->first;
 			}
 		}
 		return result;
